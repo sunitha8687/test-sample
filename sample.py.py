4e57a09747ca542d434d5a0fe18732f7d850f5e0
sunitha8687 <44788173+sunitha8687@users.noreply.github.com>
2018-11-11 22:09:38 +0100

    Update sample.py.py

--- sample.py.py
+++ sample.py.py
@@ -1,7 +1,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 25)
 server.starttls()
-server.login("dharan810938@gmail.com", "Dharan@810938")
+server.login("sunitha0594@gmail.com", "omsairam!")
 msg = "Hi"
-server.sendmail("dharan810938@gmail.com", "dharan@810938@gmail.com", msg)
+server.sendmail("sunitha0594@gmail.com", "sunitha.radhakrishnan@th-koeln.de", msg)
 
